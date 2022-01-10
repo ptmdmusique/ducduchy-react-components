92ae684f9ef5bc20d469082b1c912152511eb19f
Duc Phan <dphan@esri.com>
2022-01-10 10:39:39 -0800

    make tailwind works

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,5 +1,10 @@
+const path = require('path');
+
 module.exports = {
-  stories: ['../stories/**/*.stories.@(ts|tsx|js|jsx)'],
+  stories: [
+    '../stories/**/*.stories.@(ts|tsx|js|jsx)',
+    '../src/**/*.stories.@(ts|tsx|js|jsx)',
+  ],
   addons: [
     '@storybook/addon-links',
     '@storybook/addon-essentials',
@@ -8,5 +13,27 @@ module.exports = {
   // https://storybook.js.org/docs/react/configure/typescript#mainjs-configuration
   typescript: {
     check: true, // type-check stories during Storybook build
+  },
+  webpackFinal: async (config) => {
+    config.module.rules.push({
+      test: /\.(css|sass|scss)$/,
+      use: [
+        {
+          loader: 'postcss-loader',
+          options: {
+            postcssOptions: {
+              ident: 'postcss',
+              plugins: [
+                require('postcss-import'),
+                require('tailwindcss'),
+                require('autoprefixer'),
+              ],
+            },
+          },
+        },
+      ],
+      include: path.resolve(__dirname, '../'),
+    });
+    return config;
   },
 };
---
+++ src/components/Button/Button.tsx
@@ -0,0 +1,6 @@
+import React from 'react';
+import './Button.scss';
+
+export const Button = () => {
+  return <div className="test">Really big text</div>;
+};
