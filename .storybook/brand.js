e98f864233e6696aeb4b72199a198d1a6efe6431
Duc Phan <ptmdmusic@gmail.com>
2022-01-14 00:47:49 -0800

    change logo

---
+++ .storybook/brand.js
@@ -0,0 +1,11 @@
+// .storybook/YourTheme.js
+
+import { create } from "@storybook/theming";
+
+export default create({
+  base: "light",
+  brandTitle: "Duc Duchy - Storybook",
+  brandUrl: "https://duc-duchy.io/",
+  brandImage:
+    "https://media-exp1.licdn.com/dms/image/C5603AQGPzbtQMoC03A/profile-displayphoto-shrink_800_800/0/1583615004759?e=1647475200&v=beta&t=kaJZn2RmVloG7__GfZaXfWcLkYavbFYq5d7HSsO-hpY",
+});
--- .storybook/manager.js
+++ .storybook/manager.js
@@ -1,7 +1,9 @@
 // .storybook/manager.js
 
-import { addons } from '@storybook/addons';
+import { addons } from "@storybook/addons";
+import brand from "./brand";
 
 addons.setConfig({
-  panelPosition: 'right',
+  panelPosition: "right",
+  theme: brand,
 });
