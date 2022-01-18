a3968ecd528e9ebed130ac4f676e2556b33a9e68
Duc Phan <ptmdmusic@gmail.com>
2022-01-17 22:31:07 -0800

    fix scss "unknown word" syntax error

--- .storybook/main.js
+++ .storybook/main.js
@@ -13,46 +13,39 @@ module.exports = {
     "@storybook/addon-links",
     "@storybook/addon-essentials",
     "@storybook/preset-scss",
+    {
+      name: "@storybook/addon-postcss",
+      options: {
+        postcssLoaderOptions: {
+          implementation: require("postcss"),
+        },
+      },
+    },
+
     "storybook-addon-themes",
-    // {
-    //   name: '@storybook/addon-postcss',
-    //   options: {
-    //     postcssLoaderOptions: {
-    //       implementation: require('postcss'),
-    //       plugins: [
-    //         require('postcss-import'),
-    //         require('tailwindcss'),
-    //         require('autoprefixer'),
-    //       ],
-    //     },
-    //   },
-    // },
   ],
   // https://storybook.js.org/docs/react/configure/typescript#mainjs-configuration
   typescript: {
     check: true, // type-check stories during Storybook build
   },
   webpackFinal: async (config) => {
-    config.module.rules.push({
-      test: /\.(sass|scss)$/,
-      use: [
-        {
-          loader: "postcss-loader",
-          options: {
-            postcssOptions: {
-              ident: "postcss",
-              plugins: [
-                require("postcss-import"),
-                require("tailwindcss/nesting"),
-                require("tailwindcss"),
-                require("autoprefixer"),
-              ],
-            },
-          },
-        },
-      ],
-      include: path.resolve(__dirname, "../"),
-    });
+    // To finally fix issue with scss syntax "Unknown word"
+    // https://github.com/storybookjs/presets/issues/131#issuecomment-932282725
+    const cssRule = config.module.rules.find(
+      (rule) => rule.test && rule.test.test(".css"),
+    );
+    const sassRule = config.module.rules.find(
+      (rule) => rule.test && rule.test.test(".scss"),
+    );
+    const sassLoaderIndex = sassRule.use.findIndex(
+      ({ loader }) => loader && loader.includes("sass-loader"),
+    );
+    const postCSSLoader = cssRule.use.find(
+      ({ loader }) => loader && loader.includes("postcss-loader"),
+    );
+
+    sassRule.use.splice(sassLoaderIndex, 0, postCSSLoader);
+
     return config;
   },
 };
