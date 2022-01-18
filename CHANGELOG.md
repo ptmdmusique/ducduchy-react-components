ac90b0848d33327340d967a6f3539582bdfbe926
Duc Phan <ptmdmusic@gmail.com>
2022-01-17 23:10:19 -0800

    add home repo and some loader

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -5,6 +5,14 @@ All notable changes to this project will be documented in this file.
 The format is based on [Keep a Changelog](https://keepachangelog.com/en/1.0.0/),
 and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0.html).
 
+## [1.0.5] - 2022-17-01
+
+### Fixed
+
+- Fix circular dependency in multiple components
+- Fix scss "unknown word" when using variable interpolation or comment with `//`
+- Fix style file not built
+
 ## [1.0.1] - 2022-17-01
 
 ### Fixed
--- dts.config.js
+++ dts.config.js
@@ -1,27 +1,31 @@
 // tsdx.config.js
 
-const postcss = require('rollup-plugin-postcss');
-const sass = require('rollup-plugin-sass');
-const scss = require('rollup-plugin-scss');
+const postcss = require("rollup-plugin-postcss");
+const scss = require("rollup-plugin-scss");
 
 // Not transpiled with TypeScript or Babel, so use plain Es6/Node.js!
 module.exports = {
   // This function will run for each entry/format/env combination
   rollup(config, options) {
     config.plugins = [
-      postcss({
-        config: {
-          path: './postcss.config.js',
-        },
-        extensions: ['.css', '.sass', '.scss'],
-        minimize: true,
-        inject: {
-          insertAt: 'top',
-        },
+      // Make sure this is on top or it will fail to compile because of postcss
+      scss({
+        failOnError: true,
+        runtime: require("sass"),
       }),
 
-      scss(),
-      sass(),
+      postcss({
+        config: {
+          path: "./postcss.config.js",
+        },
+        extensions: [".css", ".sass", ".scss"],
+        minimize: true,
+        modules: true,
+        use: {
+          sass: require("sass"),
+        },
+        extract: true,
+      }),
 
       ...(config.plugins || []),
     ];
