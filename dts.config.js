d8f9245df7d44819242a141e3756b5c8fc58932c
Duc Phan <ptmdmusic@gmail.com>
2022-01-18 22:49:15 -0800

    fix css file not get built

--- dts.config.js
+++ dts.config.js
@@ -1,30 +1,23 @@
 // tsdx.config.js
 
 const postcss = require("rollup-plugin-postcss");
-const scss = require("rollup-plugin-scss");
+const sass = require("rollup-plugin-sass");
 
 // Not transpiled with TypeScript or Babel, so use plain Es6/Node.js!
 module.exports = {
   // This function will run for each entry/format/env combination
   rollup(config, options) {
     config.plugins = [
-      // Make sure this is on top or it will fail to compile because of postcss
-      scss({
-        failOnError: true,
-        runtime: require("sass"),
-      }),
-
       postcss({
-        config: {
-          path: "./postcss.config.js",
-        },
-        extensions: [".css", ".sass", ".scss"],
-        minimize: true,
-        modules: true,
-        use: {
-          sass: require("sass"),
-        },
+        preprocessor: (content, id) =>
+          new Promise((resolve, reject) => {
+            const result = sass.renderSync({ file: id });
+            resolve({ code: result.css.toString() });
+          }),
+        config: { path: "./postcss.config.js" },
+        sourceMap: true,
         extract: true,
+        extensions: [".sass", ".css"],
       }),
 
       ...(config.plugins || []),
