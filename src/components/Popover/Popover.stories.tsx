441ba4afb13472c4faedff8efdb9fdb8748f3140
Duc Phan <ptmdmusic@gmail.com>
2022-04-20 20:56:24 -0700

    finish Popover

--- src/components/Popover/Popover.stories.tsx
+++ src/components/Popover/Popover.stories.tsx
@@ -1,8 +1,26 @@
+import { Placement } from "@floating-ui/react-dom";
 import { Meta, Story } from "@storybook/react";
+import { Icon } from "../Icon";
 import { storyDisabledOption } from "../resources/story-common";
 import { Popover, PopoverProps } from "./Popover";
 
-const meta: Meta<PopoverProps> = {
+type PopoverData = PopoverProps & { placement: Placement };
+const placement = [
+  "top-start",
+  "top",
+  "top-end",
+  "right-start",
+  "right",
+  "right-end",
+  "bottom-start",
+  "bottom",
+  "bottom-end",
+  "left-start",
+  "left",
+  "left-end",
+] as const;
+
+const meta: Meta<PopoverData> = {
   title: "Components/Popover",
   component: Popover,
   argTypes: {
@@ -10,7 +28,16 @@ const meta: Meta<PopoverProps> = {
     popoverOpenerProps: storyDisabledOption,
     popoverProps: storyDisabledOption,
     popoverPanelProps: storyDisabledOption,
+    popperProps: storyDisabledOption,
     children: storyDisabledOption,
+    placement: {
+      options: placement,
+      mapping: placement.reduce((acc, cur) => ({ ...acc, [cur]: cur }), {}),
+      control: {
+        type: "select",
+        labels: placement.reduce((acc, cur) => ({ ...acc, [cur]: cur }), {}),
+      },
+    },
   },
   parameters: {
     controls: { expanded: true },
@@ -19,15 +46,46 @@ const meta: Meta<PopoverProps> = {
 
 export default meta;
 
-const Template: Story<PopoverProps> = (args) => {
+const IconWithText = ({
+  icon,
+  text,
+}: {
+  text: string;
+  icon: [string, string];
+}) => {
   return (
-    <div className="w-64 h-[20rem] flex justify-center items-center">
-      <Popover {...args} popoverOpenerProps={{ children: "Click me!" }}>
-        <div className="grid grid-cols-2">
-          <a href="/analytics">Analytics</a>
-          <a href="/engagement">Engagement</a>
-          <a href="/security">Security</a>
-          <a href="/integrations">Integrations</a>
+    <button className="flex items-center py-2 px-4 w-64 focus:outline-none focus:bg-skin-disabled hover:bg-skin-disabled hover:cursor-pointer">
+      <Icon icon={icon} className="fa-fw text-skin-secondary mr-2" />
+      <span className="ml-2 text-skin-base">{text}</span>
+    </button>
+  );
+};
+
+const Template: Story<PopoverData> = (args) => {
+  return (
+    <div className="w-[40rem] h-[30rem] flex flex-col justify-center items-center">
+      <p className="text-skin-base mb-12">
+        Try placement with screen responsive to see Popover in action!
+      </p>
+
+      <Popover
+        {...args}
+        popoverOpenerProps={{ children: "Click me!" }}
+        popperProps={{ placement: args.placement }}
+      >
+        <div className="divide-y divide-skin-disabled">
+          <div>
+            <IconWithText icon={["fas", "dog"]} text="A doggo option" />
+            <IconWithText icon={["fas", "cat"]} text="A kat option" />
+          </div>
+
+          <div>
+            <IconWithText icon={["fas", "hand-peace"]} text="A peace option" />
+          </div>
+
+          <div>
+            <IconWithText icon={["fas", "trash"]} text="Delete" />
+          </div>
         </div>
       </Popover>
     </div>
@@ -35,3 +93,74 @@ const Template: Story<PopoverProps> = (args) => {
 };
 
 export const Default = Template.bind({});
+
+const dataList = [
+  {
+    buttonText: "Solution",
+    contentList: [
+      [
+        { icon: ["fas", "dog"], text: "Doggo" },
+        { icon: ["fas", "cat"], text: "Cat" },
+        { icon: ["fas", "file-audio"], text: "Music" },
+      ],
+      [{ icon: ["fas", "trash"], text: "Delete" }],
+    ],
+  },
+  {
+    buttonText: "Something else",
+    contentList: [[{ icon: ["fas", "hand-peace"], text: "Peace" }]],
+  },
+  {
+    buttonText: "More here!",
+    contentList: [
+      [
+        { icon: ["fas", "heart"], text: "What a heart" },
+        { icon: ["fas", "spinner"], text: "Here come a spinner" },
+      ],
+      [{ icon: ["fas", "check"], text: "Check mate!" }],
+      [{ icon: ["fas", "info-circle"], text: "Hey I'm an info!" }],
+    ],
+  },
+] as const;
+
+const GroupTemplate: Story<PopoverData> = (args) => {
+  return (
+    <div className="w-[40rem] h-[30rem] flex flex-col justify-center items-center">
+      <p className="text-skin-base mb-12">
+        Try placement with screen responsive to see Popover in action!
+      </p>
+
+      <div className="flex border border-skin-base border-opacity-40 divide-x divide-skin-disabled rounded-lg">
+        {dataList.map((data, index) => (
+          <Popover
+            {...args}
+            key={index}
+            popoverOpenerProps={{
+              children: data.buttonText,
+              borderType: "plain",
+              className: "!rounded-none",
+            }}
+            popperProps={{ placement: args.placement }}
+          >
+            <div className="divide-y divide-skin-disabled">
+              {data.contentList.map((content, index) => (
+                <div key={index}>
+                  {content.map((item, index) => (
+                    <IconWithText
+                      key={index}
+                      // @ts-ignore
+                      icon={item.icon}
+                      text={item.text}
+                    />
+                  ))}
+                </div>
+              ))}
+            </div>
+          </Popover>
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export const Group = GroupTemplate.bind({});
