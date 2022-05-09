c4fb1774fff739eca58c64f1e72ec334a4c2f67b
Duc Phan <ptmdmusic@gmail.com>
2022-05-08 19:19:19 -0700

    Add MaskedInput

---
+++ src/components/MaskedInput/MaskedInput.tsx
@@ -0,0 +1,38 @@
+import IMask from "imask";
+import { forwardRef, useEffect, useRef } from "react";
+import { OmitStrict } from "../../utils/types";
+import { Input, InputProps } from "../Input";
+
+type MaskedInputProps = OmitStrict<InputProps, "onChange"> & {
+  maskOptions: IMask.AnyMaskedOptions;
+  onChange: (unmaskedValue: string, maskedValue: string) => void;
+};
+
+export const MaskedInput = forwardRef<HTMLInputElement, MaskedInputProps>(
+  ({ maskOptions, onChange, ...inputProps }, ref) => {
+    const inputRef = useRef<HTMLInputElement | null>(null);
+
+    useEffect(() => {
+      if (inputRef.current) {
+        const iMask = IMask(inputRef.current, maskOptions);
+        iMask.on("accept", () => {
+          onChange?.(iMask.unmaskedValue, iMask.value);
+        });
+
+        return () => {
+          iMask.destroy();
+        };
+      }
+    }, [maskOptions, onChange]);
+
+    return (
+      <Input
+        {...inputProps}
+        ref={(newRef) => {
+          inputRef.current = newRef;
+          ref = inputRef;
+        }}
+      />
+    );
+  },
+);
